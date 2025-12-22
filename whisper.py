6205e196f00a3a4ecce80d47606e092828fa0b5c
Denis Novikov <voothi@gmail.com>
2025-12-23 00:29:08 +0100

    20251223002907

--- whisper.py
+++ whisper.py
@@ -361,7 +361,22 @@ def restart():
 
     print(f"\nRestarting...\n")
 
-    subprocess.Popen([python, "restart.py", script_to_run] + sys.argv[1:])
+    # Filter out language arguments to always restart without specifying a language
+    new_args = []
+    i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if arg.startswith("--language="):
+            i += 1
+            continue
+        elif arg == "--language":
+            i += 2
+            continue
+        else:
+            new_args.append(arg)
+            i += 1
+
+    subprocess.Popen([python, "restart.py", script_to_run] + new_args)
     os._exit(0)
 
 
